facdc9f10ee0efa8e3ac8b389ac6f878aac12be5
AS <monu708041@gmail.com>
2026-04-18 13:10:06 +0530

    Add Firebase initialization and configuration

---
+++ firebase.js
@@ -0,0 +1,19 @@
+// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDt3TIAyxKRCe3qqaBXCxTtei7MNs_WNn4",
+  authDomain: "food-management-37781.firebaseapp.com",
+  projectId: "food-management-37781",
+  storageBucket: "food-management-37781.firebasestorage.app",
+  messagingSenderId: "714758924336",
+  appId: "1:714758924336:web:3852121d964d5145cd65f5",
+  measurementId: "G-LXDX5DGCMY"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
