25956ffde57b715404b43c5cf420249689679cb3
AS <monu708041@gmail.com>
2026-05-02 23:52:44 +0530

    Integrate Firebase Storage into firebase.js
    Added Firebase Storage functionality and exports.

--- firebase.js
+++ firebase.js
@@ -1,7 +1,38 @@
- // firebase.js
-// import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-// import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-// import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+//  // firebase.js
+// // import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+// // import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+// // import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+// // const firebaseConfig = {
+// //   apiKey: "AIzaSyDt3TIAyxKRCe3qqaBXCxTtei7MNs_WNn4",
+// //   authDomain: "food-management-37781.firebaseapp.com",
+// //   projectId: "food-management-37781",
+// //   storageBucket: "food-management-37781.firebasestorage.app",
+// //   messagingSenderId: "714758924336",
+// //   appId: "1:714758924336:web:3852121d964d5145cd65f5",
+// //   measurementId: "G-LXDX5DGCMY"
+// // };
+
+// // const app = initializeApp(firebaseConfig);
+
+// // export const auth = getAuth(app);
+// // export const db = getFirestore(app);
+
+
+// // // firebase.js
+// // import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+// // import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+// // import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+// // const firebaseConfig = {
+// //   apiKey: "YOUR_KEY",
+// //   authDomain: "YOUR_PROJECT.firebaseapp.com",
+// //   projectId: "YOUR_PROJECT_ID",
+// // };
+// // Import the functions you need from the SDKs
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+// import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+// import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
 // const firebaseConfig = {
 //   apiKey: "AIzaSyDt3TIAyxKRCe3qqaBXCxTtei7MNs_WNn4",
@@ -13,26 +44,15 @@
 //   measurementId: "G-LXDX5DGCMY"
 // };
 
+// // Initialize Firebase
 // const app = initializeApp(firebaseConfig);
-
 // export const auth = getAuth(app);
 // export const db = getFirestore(app);
-
-
-// // firebase.js
-// import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-// import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-// import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-
-// const firebaseConfig = {
-//   apiKey: "YOUR_KEY",
-//   authDomain: "YOUR_PROJECT.firebaseapp.com",
-//   projectId: "YOUR_PROJECT_ID",
-// };
-// Import the functions you need from the SDKs
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+// ADDED: Import Firebase Storage
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-storage.js"; 
 
 const firebaseConfig = {
   apiKey: "AIzaSyDt3TIAyxKRCe3qqaBXCxTtei7MNs_WNn4",
@@ -48,8 +68,10 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+// ADDED: Initialize and export Storage
+export const storage = getStorage(app);
 
-// const app = initializeApp(firebaseConfig);
+// // const app = initializeApp(firebaseConfig);
 
-// export const auth = getAuth(app);
-// export const db = getFirestore(app);
+// // export const auth = getAuth(app);
+// // export const db = getFirestore(app);
